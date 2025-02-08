f0b6cd72709ce06410284882f3a360b6f89cf0ea
Soumalya Biswas <soumalyaabiswas2001@gmail.com>
2025-02-09 03:36:37 +0530

    new branch

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,14 +16,20 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+const corsOptions = {
+  origin: process.env.NODE_ENV === "production" ? "https://your-frontend.vercel.app" : "http://localhost:5174",
+  credentials: true,
+};
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
